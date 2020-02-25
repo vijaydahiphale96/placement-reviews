0f5e5e77621f70f4680f683f007b7de546e54861
Vijay Dahiphale <vijay.dahiphale@msm.digital>
2020-02-25 19:13:15 +0530

    added rest-api service

--- src/app/shared/enums/headers.enum.ts
+++ src/app/shared/enums/headers.enum.ts
@@ -1,10 +1,13 @@
 export enum HeaderKeys {
+  CONTENT_TYPE = 'Content-Type',
   AUTH = 'auth',
   ACCESS_TOKEN = 'accessToken',
-  SHOW_LOADER = 'showLoader'
+  SHOW_LOADER = 'showLoader',
+  DISPLAY_ERROR = 'displayError'
 }
 
 export enum HeaderKeyValues {
   BOOLEAN_TRUE = 'True',
-  BOOLEAN_FALSE = 'False'
+  BOOLEAN_FALSE = 'False',
+  CONTENT_TYPE_JSON = 'application/json'
 }
---
+++ src/app/shared/services/rest-api.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { HeaderKeys, HeaderKeyValues } from '../enums/headers.enum';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestApiService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  post<T>(
+    url: string,
+    postparams: any,
+    isAuthorizationRequired: boolean,
+    showLoader: boolean,
+    displayError: boolean,
+  ): Observable<T> {
+
+    const commonHttpHeaderOptions = this.getHttpHeaders(isAuthorizationRequired, showLoader, displayError)
+    console.log(`${commonHttpHeaderOptions}`)
+    return this.http.post<T>(url, postparams, commonHttpHeaderOptions);
+  }
+
+  put<T>(
+    url: string,
+    postparams: any,
+    isAuthorizationRequired: boolean,
+    showLoader: boolean,
+    displayError: boolean,
+  ): Observable<T> {
+    const commonHttpHeaderOptions = this.getHttpHeaders(isAuthorizationRequired, showLoader, displayError)
+    return this.http.put<T>(url, postparams, commonHttpHeaderOptions);
+  }
+
+  get<T>(
+    url: string,
+    isAuthorizationRequired: boolean,
+    showLoader: boolean,
+    displayError: boolean,
+  ): Observable<T> {
+    const commonHttpHeaderOptions = this.getHttpHeaders(isAuthorizationRequired, showLoader, displayError)
+    return this.http.get<T>(url, commonHttpHeaderOptions);
+  }
+
+  getHttpHeaders(isAuthorizationRequired: boolean, showLoader: boolean, displayError: boolean) {
+    return {
+      headers: new HttpHeaders({
+        [HeaderKeys.CONTENT_TYPE]: HeaderKeyValues.CONTENT_TYPE_JSON,
+        [HeaderKeys.AUTH]: isAuthorizationRequired === true ? HeaderKeyValues.BOOLEAN_TRUE : HeaderKeyValues.BOOLEAN_FALSE,
+        [HeaderKeys.SHOW_LOADER]: showLoader === true ? HeaderKeyValues.BOOLEAN_TRUE : HeaderKeyValues.BOOLEAN_FALSE,
+        [HeaderKeys.DISPLAY_ERROR]: displayError === true ? HeaderKeyValues.BOOLEAN_TRUE : HeaderKeyValues.BOOLEAN_FALSE
+      })
+    };
+  }
+
+}
