2575b1b86dbc25d2bf9a991562a8e5a1ec4cf23f
Vijay Dahiphale <vijay.dahiphale@msm.digital>
2020-03-06 19:05:53 +0530

    removed cloning of req in auth interceptor

--- src/app/shared/interceptors/auth.interceptor.ts
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -8,24 +8,19 @@ import {
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { UserDataService } from '../services/user-data.service';
-import { Router } from '@angular/router';
-import { MainRoutes } from '../enums/routes.enum';
 import { HeaderKeys, HeaderKeyValues } from '../enums/headers.enum';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
   constructor(
-    private router: Router,
     private userDataService: UserDataService
   ) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     if (request.headers.get(HeaderKeys.AUTH) === HeaderKeyValues.BOOLEAN_TRUE) {
-      const clonedRequest = request.clone({
-        headers: request.headers.set(HeaderKeys.ACCESS_TOKEN, this.userDataService.accessToken)
-      });
-      return next.handle(clonedRequest);
+      request.headers.set(HeaderKeys.ACCESS_TOKEN, this.userDataService.accessToken);
+      return next.handle(request);
     } else {
       return next.handle(request);
     }
